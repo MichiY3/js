f517499ac8f7b04b83aee4184bbed6e8b30c4de4
MichiY3 <michiyoy@hotmail.com>
2020-10-28 19:36:40 +0900

    クラス名簿リスト

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ console.log(CapitalCities['America']);
 // リストの4番目の人の趣味をconsoleで表示しましょう
 
 let StudentList = [
-{name:'Erikoーsennsei', from:'青森', hobby:'旅'},
 {name:'Ota-san', from:'宮古島', hobby:'ゲームとギター'},
 {name:'Shimajiri-san', from:'浦添',hobby:'ゴルフ'},
 {name:'Maemoto-san', from:'北谷',hobby:'バスケット'},
@@ -74,13 +73,160 @@ console.log(StudentList[0]['hobby']);
 
 // ５次元配列　入れ子５個！
 
-let creature =['魚類','両生類','爬虫類','鳥類','哺乳類'];
+
+// 条件分岐
+	// 　比較演算子
+
+// 	/練習問題
+// 変数weightに任意の数字を代入してください。
+// weightが200以上の場合はヘビー級
+// weightが115以上、118以下の場合はバンタム級
+// それ以外の場合はその他の階級
+// とconsoleに表示してください。
+
+let weight = 119;
+
+if(weight >= 200){
+	console.log('ヘビー級');
+}else if(weight >= 115 && weight <= 118 ){
+	console.log('バンタム級');
+}else{
+	console.log('その他の階級');
+}
 
 
 
+// switch文
+// 練習問題
+// 変数greetingに任意の文字列を代入してください。
+// greetingがおはようの場合は日本語
+// greetingがgood morningの場合は英語 
+// それ以外の場合は?
+// とconsoleに表示してください。
+
+let greeting ='buenos dias';
+
+switch(greeting){
+	case'おはよう':
+		console.log('日本語');
+		break; 
+			// ここで終わりの印、必ずかく
+	case'good morning':
+		console.log('english');
+		break;
+	default:
+		console.log('?');
+		break;
+}
 
 
 
+// 繰り返し
+
+/*
+	1から10までの数字を表示するプログラム
+	変数iに1を代入する(初期化式)
+	iが10以下の間だけ繰り返す(条件式)
+	iを1回ごとに+1する(変化式)
+*/
+for (let i = 1; i <= 10; i++) {
+	console.log('繰り返しますよ');
+    console.log(i);
+}
+
+// 練習問題
+// 以下の条件にあうfor文を書いてください。
+// 初期化式で変数iに2を代入する
+// iが20以下の間繰り返す
+// 1回ごとにiを1増やす
+// 繰り返しのたびにiをconsoleに表示する
+for (let i = 2; i <= 20; i++){
+	console.log(i);
+}
+
+// 練習問題
+// 以下の条件にあうwhile文を書いてください。
+// 変数kに2を代入する
+// kが20以下の間繰り返す
+// 1回ごとにkを1増やす
+// 繰り返しのたびにkをconsoleに表示する
+
+for (let k = 2; k <= 20; k++){
+	console.log(k);
+}
+
+
+// for文を使って下記リストのname部分をconsoleに表示
+// ヒント：forで数が変わるiは、○番目の指定にもつかえます。
+
+let StudentList2 = [
+{name:'Ota-san', from:'宮古島', hobby:'ゲームとギター'},
+{name:'Shimajiri-san', from:'浦添',hobby:'ゴルフ'},
+{name:'Maemoto-san', from:'北谷',hobby:'バスケット'},
+{name:'Takushi-san', from:'うるま市',hobby:'サッカーとファッション'},
+{name:'Igei-san', from:'沖縄',hobby:'旅'},
+{name:'Erikoーsennsei', from:'青森',hobby:'食べ歩きと旅'},
+{name:'Seedくん', from:'セブ島',hobby:'サーフィン'}
+];
+
+for (let i=0; i <=5; i++){
+	 console.log(StudentList2[i].name);
+}
+
+// for (let i = StudentList2.name; i <= 7; i++){
+// 	console.log(i);
+// }
+
+// console.log(StudentList[0].name);
+// console.log(StudentList[1].name);
+// console.log(StudentList[2].name);
+// console.log(StudentList[3].name);
+// console.log(StudentList[4].name);
+// console.log(StudentList[5].name);
+// console.log(StudentList[6].name);
+
+
+
+// 繰り返しの中断、スキップ
+
+//1 - 30までの数値で、3の倍数ではないものを表示するプログラム
+//3の倍数の時だけ処理が不要なため、continueでスキップする。
+for (let i = 1; i <= 30; i++) {
+    if (i % 3 === 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+// 練習問題
+
+// 以下の条件にあうfor文を書いてください。
+// 初期化式で変数iに1を代入する
+// iが50以下の間繰り返す 
+// 1回ごとにiを1増やす
+// 繰り返しのたびにiをconsoleに表示する
+// ただし、iが5の倍数でない場合は表示しない。
+
+
+for( let i = 1; i <= 50; i++){
+	if (i % 5 === 0){
+	console.log(i);
+	}else{
+		continue;
+	}
+}
+
+// ランダム変数
+console.log('ランダム変数');
+var random = Math.floor( Math.random() * 11 );
+console.log( random );
+
+var random = Math.floor(Math.random() * 3);
+let omikuji =[
+'大吉',
+'中吉',
+'小吉'];
+console.log(omikuji[random]);
 
 
 
